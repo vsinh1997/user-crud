c6e670f3746b61d7cab65c62c18df8d940a61409
Nguyen Canh Duong <canhduong921999@gmail.com>
2024-09-29 10:02:48 +0900

    first commit

---
+++ src/test/java/com/example/usercrud/UserCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
